0e4984940c5ce67fffeee117a44d0c46b00c7914
solferse93 <solferse93@gmail.com>
2016-09-03 16:49:11 +0200

    Se modifica todo el menu desplegable para que ahora si se mueva por los diferentes Layout

--- app/src/main/java/com/scolti/informes_app/MainActivity.java
+++ app/src/main/java/com/scolti/informes_app/MainActivity.java
@@ -1,16 +1,9 @@
 package com.scolti.informes_app;
 
-import android.content.ActivityNotFoundException;
-import android.content.Intent;
-import android.database.Cursor;
-import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.Bundle;
-import android.os.Environment;
-import android.provider.MediaStore;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.view.View;
+//import android.support.design.widget.FloatingActionButton;
+import android.support.v4.app.Fragment;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
@@ -19,34 +12,10 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.ImageView;
-import android.widget.Toast;
-
-import com.itextpdf.text.Document;
-import com.itextpdf.text.DocumentException;
-import com.itextpdf.text.Paragraph;
-import com.itextpdf.text.pdf.PdfWriter;
-
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
 
 public class MainActivity extends AppCompatActivity
-        implements NavigationView.OnNavigationItemSelectedListener {
-
-
-    private static int RESULT_LOAD_IMAGE = 1;
-
-    EditText et_titulo;
-    EditText et_realizador;
-    EditText et_escenario;
-    EditText et_localizacion;
-    EditText et_pag_guion;
-    EditText et_secuencias;
-    EditText et_permisos;
-    ImageView ImgView;
+        implements NavigationView.OnNavigationItemSelectedListener,
+        Trabajos.OnFragmentInteractionListener, Amigos.OnFragmentInteractionListener, Ubicaciones.OnFragmentInteractionListener, Informes.OnFragmentInteractionListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -72,28 +41,6 @@ public class MainActivity extends AppCompatActivity
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
 
-        et_titulo = (EditText) findViewById(R.id.et_titulo);
-        et_realizador = (EditText) findViewById(R.id.et_realizador);
-        et_escenario = (EditText) findViewById(R.id.et_escenario);
-        et_localizacion = (EditText) findViewById(R.id.et_localizacion);
-        et_pag_guion = (EditText) findViewById(R.id.et_pag_guion);
-        et_secuencias = (EditText) findViewById(R.id.et_secuencias);
-        et_permisos = (EditText) findViewById(R.id.et_permisos);
-        ImgView = (ImageView) findViewById(R.id.ImgView);
-
-        Button buttonLoadImage = (Button) findViewById(R.id.btn_img);
-        buttonLoadImage.setOnClickListener(new View.OnClickListener() {
-
-            @Override
-            public void onClick(View arg0) {
-
-                Intent i = new Intent(
-                        Intent.ACTION_PICK,
-                        android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
-
-                startActivityForResult(i, RESULT_LOAD_IMAGE);
-            }
-        });
 
     }
 
@@ -115,30 +62,6 @@ public class MainActivity extends AppCompatActivity
     }
 
     @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-
-        if (requestCode == RESULT_LOAD_IMAGE && resultCode == RESULT_OK && null != data) {
-            Uri selectedImage = data.getData();
-            String[] filePathColumn = {MediaStore.Images.Media.DATA};
-
-            Cursor cursor = getContentResolver().query(selectedImage,
-                    filePathColumn, null, null, null);
-            cursor.moveToFirst();
-
-            int columnIndex = cursor.getColumnIndex(filePathColumn[0]);
-            String picturePath = cursor.getString(columnIndex);
-            cursor.close();
-
-            ImageView imageView = (ImageView) findViewById(R.id.ImgView);
-            imageView.setImageBitmap(BitmapFactory.decodeFile(picturePath));
-
-        }
-
-
-    }
-
-    @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
@@ -157,18 +80,33 @@ public class MainActivity extends AppCompatActivity
 
     @Override
     public boolean onNavigationItemSelected(MenuItem item) {
-        // Handle navigation view item clicks here.
+        // En el menú desplegable, cuando seleccione uno de los apartados vaya a ese layout
         int id = item.getItemId();
 
-        if (id == R.id.nav_home) {
+        Fragment fragment = null;
+        Boolean FragmentosSeleccionado=false;
 
-        } else if (id == R.id.nav_folder) {
+        if (id == R.id.nav_inicio) {
 
-        } else if (id == R.id.nav_friends) {
+        } else if (id == R.id.nav_trabajos) {
 
-        } else if (id == R.id.nav_location) {
+            fragment = new Trabajos();
+            FragmentosSeleccionado=true;
 
-        } else if (id == R.id.nav_document) {
+        } else if (id == R.id.nav_amigos) {
+
+            fragment = new Amigos();
+            FragmentosSeleccionado=true;
+
+        } else if (id == R.id.nav_ubicaciones) {
+
+            fragment = new Ubicaciones();
+            FragmentosSeleccionado=true;
+
+        } else if (id == R.id.nav_informes) {
+
+            fragment = new Informes();
+            FragmentosSeleccionado=true;
 
         } else if (id == R.id.nav_share) {
 
@@ -176,76 +114,18 @@ public class MainActivity extends AppCompatActivity
 
         }
 
+        if (FragmentosSeleccionado){
+            getSupportFragmentManager().beginTransaction().replace(R.id.Contenedor, fragment).commit();
+        }
+
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
     }
 
-    public void createPDF(View view) {
-        EditText txt = (EditText) findViewById(R.id.et_titulo);
-        EditText txt2 = (EditText) findViewById(R.id.et_realizador);
-        EditText txt3 = (EditText) findViewById(R.id.et_escenario);
-        EditText txt4 = (EditText) findViewById(R.id.et_localizacion);
-        EditText txt5 = (EditText) findViewById(R.id.et_pag_guion);
-        EditText txt6 = (EditText) findViewById(R.id.et_secuencias);
-        EditText txt7 = (EditText) findViewById(R.id.et_permisos);
-        ImageView img = (ImageView) findViewById(R.id.ImgView);
 
-        Document doc = new Document();
+    @Override
+    public void onFragmentInteraction(Uri uri) {
 
-        final String outPath = Environment.getExternalStorageDirectory() + "/informe.pdf";
-        try {
-            PdfWriter.getInstance(doc, new FileOutputStream(outPath));
-            doc.open();
-
-            //Dos maneras de hacerlo:
-            //1. Paragraph p=new Paragraph("Título: "+txt.getText().toString());
-            //1.2 doc.add(p);
-            //2. doc.add(new Paragraph("Título: "+(txt.getText().toString())));
-
-            doc.add(new Paragraph("Título: " + (txt.getText().toString())));
-            doc.add(new Paragraph(" "));
-            doc.add(new Paragraph("Realizador: " + (txt2.getText().toString())));
-            doc.add(new Paragraph(" "));
-            doc.add(new Paragraph("Escenario: " + (txt3.getText().toString())));
-            doc.add(new Paragraph(" "));
-            doc.add(new Paragraph("Localización: " + (txt4.getText().toString())));
-            doc.add(new Paragraph(" "));
-            doc.add(new Paragraph("Página guión: " + (txt5.getText().toString())));
-            doc.add(new Paragraph(" "));
-            doc.add(new Paragraph("Secuencias: " + (txt6.getText().toString())));
-            doc.add(new Paragraph(" "));
-            doc.add(new Paragraph("Permisos: " + (txt7.getText().toString())));
-            doc.add(new Paragraph(" "));
-
-            doc.add(new Paragraph(" "));
-            doc.close();
-        } catch (DocumentException e) {
-            e.printStackTrace();
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        }
-
-       //Toast.makeText(getBaseContext(), "El PDF se ha generado con éxito :)", Toast.LENGTH_LONG).show();
-        Snackbar.make(view, "El PDF se ha generado con éxito :)", Snackbar.LENGTH_LONG).setActionTextColor(getResources()
-                .getColor(R.color.snackbar_action)).setAction("VER",new View.OnClickListener(){
-            @Override
-            public void onClick(View view){
-                //Acciones que ocurren al pulsar sobre "VER"
-                File file = new File (outPath);
-                if(file.exists()){
-                    Uri path = Uri.fromFile(file);
-                    Intent intent = new Intent(Intent.ACTION_VIEW);
-                    intent.setDataAndType(path,"application/pdf");
-                    intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-                    try{
-                        startActivity(intent);
-                    }
-                    catch (ActivityNotFoundException e){
-                        Toast.makeText(getBaseContext(),"No pudo abrirse el archivo PDF", Toast.LENGTH_SHORT).show();
-                    }
-                }
-            }
-        }).show();
     }
 }
